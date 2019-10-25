e5ebc52e9dad1fcd8593a3b41b8c8c19768b00e8
Киселев Иван <47575805+Kekcek@users.noreply.github.com>
2019-10-25 13:18:35 +0300

    Update main.c
    Исправил вывод, он выводил сначала одну, потом две, потом три и так далее. Исправил запись указателей

--- LABA 9/main.c
+++ LABA 9/main.c
@@ -1,84 +1,124 @@
 #pragma warning(disable : 4996) //необходимо для использования устаревших функций (scanf)
 #include <stdio.h>
-
-/*определение символических констант*/
-#define YES 1
-#define NO 0
-#define MAXLINE 1024  // максимальная длина массива
+#define MAXLINE 1024 // максимальная длина массива
 
 int main(void)
 {
 	FILE *fpin;
 	FILE *fpout;
 	char line[MAXLINE];
-	char mine[MAXLINE] = "";
+	char mine[MAXLINE];
 	char *ptr;
 	char *ptr2 = mine;
 
 	fpin = fopen("C:\\Users\\ksal\\source\\repos\\LABA9\\file.txt", "rt");
-		if (fpin == NULL)  
-		return;  // ошибка при открытии файла 
+	if (fpin == NULL)
+		return 0; // ошибка при открытии файла
 
 	fpout = fopen("C:\\Users\\ksal\\source\\repos\\LABA9\\result.txt", "wt");
-		if (fpout == NULL)
-		return;  // ошибка при открытии файла 
+	if (fpout == NULL)
+		return 0; // ошибка при открытии файла
 
-		while (!feof(fpin))// цикл до конца файла  
+	while (!feof(fpin))// цикл до конца файла
+	{
+
+		ptr = fgets(line, MAXLINE, fpin); //чтение строки
+		
+		if (ptr == NULL)
+			break; // файл исчерпан
+		
+
+		while (*ptr != '\0') // цикл до конца строки
 		{
-			ptr = fgets(line, MAXLINE, fpin); //чтение строки
-			if (ptr == NULL)
-				break; // файл исчерпан
-
-			while (*ptr != '\0') // цикл до конца строки 
+			if (*ptr == '1' || *ptr == '2' || *ptr == '3' || *ptr == '4' || *ptr == '5' || *ptr == '6' || *ptr == '7' || *ptr == '8' || *ptr == '9' || *ptr == '0')
 			{
-				if (*ptr == '1' || *ptr == '2' || *ptr == '3' || *ptr == '4' || *ptr == '5' || *ptr == '6' || *ptr == '7' || *ptr == '8' || *ptr == '9' || *ptr == '0'  )
+				if (*ptr == '1')
 				{
-					if (*ptr == '1')
-					{
-						*ptr2++ = 'O';
-						*ptr2++ = 'n';
-						*ptr2++ = 'e';
-					}
-					if (*ptr == '2')
-					{
-						*ptr2++ = 'T';
-						*ptr2++ = 'w';
-						*ptr2++ = 'o';
+					*ptr2++ = 'O';
+					*ptr2++ = 'n';
+					*ptr2++ = 'e';
+				}
+				if (*ptr == '2')
+				{
+					*ptr2++= 'T';
+					*ptr2++ = 'w';
+					*ptr2++ = 'o';
 
-					}
-					if (*ptr == '3')
-					{
-						*ptr2++ = 't';
-						*ptr2++ = 'h';
-						*ptr2++ = 'r';
-						*ptr2++ = 'e';
-						*ptr2++ = 'e';
-					}
-					if (*ptr == '4')
-					{
-						*ptr2++ = 'f';
-						*ptr2++ = 'o';
-						*ptr2++ = 'u';
-						*ptr2++ = 'r';
-					}
-					if (*ptr == '5')
-					{
-						*ptr2++ = 'f';
-						*ptr2++ = 'i';
-						*ptr2++ = 'v';
-						*ptr2++ = 'e';
-					}
 				}
-				else 
+				if (*ptr == '3')
 				{
-					ptr2 = *ptr;
+					*ptr2++ = 'T';
+					*ptr2++ = 'h';
+					*ptr2++ = 'r';
+					*ptr2++ = 'e';
+					*ptr2++ = 'e';
 				}
-				*ptr2++;
-				*ptr++;
+				if (*ptr == '4')
+				{
+					*ptr2++ = 'F';
+					*ptr2++ = 'o';
+					*ptr2++ = 'u';
+					*ptr2++ = 'r';
+				}
+				if (*ptr == '5')
+				{
+					*ptr2++ = 'F';
+					*ptr2++ = 'i';
+					*ptr2++ = 'v';
+					*ptr2++ = 'e';
+				}
+				if (*ptr == '6')
+				{
+					*ptr2++ = 'S';
+					*ptr2++ = 'i';
+					*ptr2++ = 'x';
+				}
+				if (*ptr == '7')
+				{
+					*ptr2++ = 'S';
+					*ptr2++ = 'e';
+					*ptr2++ = 'v';
+					*ptr2++ = 'e';
+					*ptr2++ = 'n';
+				}
+				if (*ptr == '8')
+				{
+					*ptr2++ = 'E';
+					*ptr2++ = 'i';
+					*ptr2++ = 'g';
+					*ptr2++ = 'h';
+					*ptr2++ = 't';
+				}
+				if (*ptr == '9')
+				{
+					*ptr2++ = 'N';
+					*ptr2++ = 'i';
+					*ptr2++ = 'n';
+					*ptr2++ = 'e';
+				}
+				if (*ptr == '0')
+				{
+					*ptr2++ = 'Z';
+					*ptr2++ = 'e';
+					*ptr2++ = 'r';
+					*ptr2++ = 'o';
+				}
 			}
-			fputs(mine, fpout); // запись строки 
+			else
+			{
+				*ptr2= *ptr;
+				*ptr2++;
+			}
+			
+			ptr++;
 		}
-		fclose(fpin); // закрыть входной файл  
-		fclose( fpout ); // закрыть выходной файл 
-		return 0;
-}			
+		*ptr2 = '\0';
+		fputs(mine, fpout); // запись строки
+		
+		
+	}
+	fputs(mine, fpout); // запись строки
+	fclose(fpin); // закрыть входной файл
+	fclose(fpout); // закрыть выходной файл
+	return 0;
+}
